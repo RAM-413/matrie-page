af4d45963a4bd7c1a2a9d5ae726d358786c0aba3
RAM-413 <ramsharma22422@gmail.com>
2023-09-25 10:41:37 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+$('.slide').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '.buttonprev',
+    nextArrow: '.buttonnxt',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+
+
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+
+        }
+    },
+    ]
+});
